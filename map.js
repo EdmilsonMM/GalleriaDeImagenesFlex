7761f4b97d824defd1f60d193a65652c149d7e73
Edmilson <chvez4012@gmail.com>
2022-01-18 15:33:37 -0500

    v.1

---
+++ map.js
@@ -0,0 +1,23 @@
+
+/*function GoMapa(){
+    var cordenadas = {lat:-12.042524151579277 , lng:-76.93712175966618};
+    var map = new google.maps.Map(document.getElementById('map'),{
+        zoom:10,
+        center:cordenadas
+    });
+}*/
+
+/*
+
+function iniciarMap(){
+    var coord = {lat:-34.5956145 ,lng: -58.4431949};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
+*/
